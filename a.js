ea5f00cd5e125c914e1ab4e57bd76632067c3e5d
dug duy <dugduy01@gmail.com>
2022-06-01 10:34:44 +0700

    try requests

---
+++ a.js
@@ -0,0 +1,20 @@
+fetch("https://apiptb.5phutthuocbai.com/api/ReviewAll/question/create", {
+  "headers": {
+    "accept": "application/json,application/x-www-form-urlencoded,text/plain,*/*",
+    "accept-language": "vi,fr-FR;q=0.9,fr;q=0.8,en-US;q=0.7,en;q=0.6",
+    "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9naXZlbm5hbWUiOiJBZG1pbiBUcuG6r2MgTmdoaeG7h20gVG_DoG4gRGnhu4duIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMTU1NjkiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW50bnRkIiwiZXhwIjoxNzE2ODk0ODQ3LCJpc3MiOiJodHRwczovL3NpZXV0cmluaG9ob2NkdW9uZy5jb20iLCJhdWQiOiJodHRwczovL3NpZXV0cmluaG9ob2NkdW9uZy5jb20ifQ.qMd1d6ue-ZUxZzE01rKDXyFhtWND1c6xUiv_87KrIEg",
+    "content-type": "application/json;charset=UTF-8",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"CocCoc\";v=\"100\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+  },
+  "referrer": "https://admintracnghiemtoandien.stnhd.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"class_id\":5,\"course_id\":152,\"topics_id\":\"TV02-025\",\"knowledge_topic_Id\":3393,\"question\":\"<p>Từ nào dưới đây thuộc nhóm từ chỉ sự vật: Thư viện, thủ thư, đọc, tàu biển, nằm im, băng qua, xe buýt, lạc đà.</p>\",\"point\":1,\"level\":\"level2\",\"time_second\":60,\"status\":1,\"explain_the_answer\":\"<p>Vì từ chỉ sự vật là những danh từ chỉ con người, cây cối, đồ vật hay các hiện tượng, khái niệm như ba, mẹ, con voi, cái cây, đôi đũa,…</p>\",\"listAnswer\":[{\"title\":\"A\",\"answer_flag\":false,\"status\":1,\"sort_order\":1,\"answer\":\"<p>thư viện, thủ thư, đọc.</p>\"},{\"title\":\"B\",\"answer_flag\":true,\"status\":1,\"sort_order\":2,\"answer\":\"<p>thư viện, thủ thư, tàu biển, xe buýt, lạc đà.</p>\"},{\"title\":\"C\",\"answer_flag\":false,\"status\":1,\"sort_order\":3,\"answer\":\"<p>đọc, nằm im, băng qua.</p>\"},{\"title\":\"D\",\"answer_flag\":false,\"status\":1,\"sort_order\":4,\"answer\":\"<p>thủ thư, lạc đà, băng qua.</p>\"}],\"name_subject_topic\":\"MRVT: Giao tiếp, kết nối\",\"name_topics\":\"TV02-025\",\"name_class\":\"Lớp 2\",\"name_course\":\"Tiếng Việt\",\"user_created\":15569}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
