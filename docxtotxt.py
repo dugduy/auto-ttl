f6a7ba37c3ac341023db322bd4d7465108ead0fa
dug duy <dugduy01@gmail.com>
2022-06-16 20:23:50 +0700

    update dtt.py

--- docxtotxt.py
+++ docxtotxt.py
@@ -3,4 +3,4 @@ doc = Document('./Bản sao của T10-004-04.docx')
 f=open('T10-004-04.txt','w',encoding='utf-8')
 for para in doc.paragraphs:
     print(para.text)
-    f.write(para.text)
+    f.write(para.text+'\n')
